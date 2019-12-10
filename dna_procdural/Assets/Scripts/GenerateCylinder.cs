f4b313223d522ba56d62edbc4639f7d1adc76694
ashrafuz <rawnok02@gmail.com>
2019-12-10 20:52:21 +0600

    no message

--- dna_procdural/Assets/Scripts/GenerateCylinder.cs
+++ dna_procdural/Assets/Scripts/GenerateCylinder.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class GenerateCylinder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnDrawGizmosSelected() {
+        Debug.Log("selected");
     }
 }
