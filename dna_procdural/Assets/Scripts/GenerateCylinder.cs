885904019fe9eef8e698509075dc06d07a3fc94e
ashrafuz <rawnok02@gmail.com>
2019-12-15 03:34:38 +0600

    bezier working

--- dna_procdural/Assets/Scripts/GenerateCylinder.cs
+++ dna_procdural/Assets/Scripts/GenerateCylinder.cs
@@ -2,9 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenerateCylinder : MonoBehaviour
-{
-    private void OnDrawGizmosSelected() {
-        Debug.Log("selected");
+public class GenerateCylinder : MonoBehaviour {
+    private void OnDrawGizmosSelected () {
+        //MyGizmos.DrawWireCirlce (transform.position, Quaternion.identity, 10);
+        Init();
+        GenerateMesh();
     }
+
+    private Mesh mMesh;
+    [SerializeField][Range (1, 100)]  private float mRadius = 2;
+
+    [SerializeField][Range (8, 128)] private int m_Detail = 8;
+
+    private void Awake () {
+        Init();
+    }
+
+    private void Init(){
+        mMesh = new Mesh ();
+        mMesh.name = "Cylinder Quad";
+        GetComponent<MeshFilter> ().sharedMesh = mMesh;
+    }
+
+    private void GenerateMesh () {
+        mMesh.Clear ();
+
+        List<Vector3> vertexList = new List<Vector3> ();
+        List<Vector3> normalList = new List<Vector3> ();
+        List<Vector2> uvs = new List<Vector2> ();
+
+        for (int i = 0; i < m_Detail + 1; i++) {
+            float t = i / (float) m_Detail;
+            float angleInRad = Maths.TAU * t;
+
+            Vector2 dir = new Vector2();
+            dir.x = Mathf.Cos(angleInRad);
+            dir.y = Mathf.Sin(angleInRad);
+
+            Vector2 pos = mRadius * dir;
+            Gizmos.DrawWireSphere(pos, 0.1f);
+
+            vertexList.Add(pos);
+
+            vertexList.Add(transform.position);
+        }
+
+        List<int> triangleIndices = new List<int> ();
+
+        for (int i = 0; i < m_Detail; i++)
+        {
+            int rootIndex = i * 2;
+            int rootBotIndex = rootIndex+1;
+            int rootBotNextIndex = rootIndex + 3;
+            int rootTopNextIndex =rootIndex + 2;
+
+            triangleIndices.Add(rootIndex);
+            triangleIndices.Add(rootBotIndex);
+            triangleIndices.Add(rootBotNextIndex);
+
+            triangleIndices.Add(rootBotNextIndex);
+            triangleIndices.Add(rootTopNextIndex);
+            triangleIndices.Add(rootIndex);
+        }
+
+        mMesh.SetVertices(vertexList);
+        mMesh.SetTriangles(triangleIndices,0);
+
+        mMesh.RecalculateNormals();
+    }
+
 }
---
+++ dna_procdural/Assets/Scripts/MyGizmos.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Maths {
+    public const float TAU = 6.283185f;
+
+    public static Vector2 GetUnitVectorByAngle (float _angRad) {
+        return new Vector2 (Mathf.Cos (_angRad), Mathf.Sin (_angRad));
+    }
+
+}
+
+public static class MyGizmos {
+    public static void DrawWireCirlce (Vector3 _pos, Quaternion _rot, float _radius, int _detail = 32) {
+        Vector3[] points = new Vector3[_detail];
+
+        for (int i = 0; i < _detail; i++) {
+            float t = i / (float) _detail;
+            float angleInRad = t * Maths.TAU;
+
+            Vector2 point2d = Maths.GetUnitVectorByAngle (angleInRad) * _radius;
+            points[i] = _pos + _rot * point2d; //vector transformation
+        }
+
+        for (int i = 0; i < _detail - 1; i++) {
+            Gizmos.DrawLine (points[i], points[i + 1]);
+            Gizmos.DrawWireSphere(points[i], 0.2f);
+        }
+        Gizmos.DrawLine (points[_detail - 1], points[0]);
+
+        //Vector3[] ponts3d = new Vector3[_detail];
+    }
+}
